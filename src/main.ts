cc083e5b72978bfee4f3aac15d4fbc0663419fe5
Oscar Bejarano <obejarano@arrivelogistics.com>
2019-10-08 12:12:32 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/exception-filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const options = new DocumentBuilder()
